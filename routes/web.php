d33e927bc2decb5868226a21d75a470fa9732eed
Irina <ten7473@mail.ru>
2023-01-31 23:20:52 +0500

    auth

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
 use App\Http\Controllers\FieldController;
-use App\Http\Controllers\PrimeController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TeamController;
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,12 +22,12 @@ use Illuminate\Support\Facades\Route;
     return view('welcome');
 });*/
 
-//Route::get('/hello', [TestController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-//Route::get('/', [PrimeController::class, 'index']);
+require __DIR__.'/auth.php';
 
-Route::get('/', [UserController::class, 'index']);
-Route::resource('users', UserController::class);
 
 Route::get('/field', [FieldController::class, 'index']);
 Route::resource('fields', FieldController::class);
@@ -46,6 +43,3 @@ Route::resource('projects', ProjectController::class);
 
 //Route::get('/task', [TaskController::class, 'index']);
 Route::resource('tasks', TaskController::class);
-
-
-
